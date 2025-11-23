f662c8371aefb68ad6488505a819580221feaba3
yogevh022 <yogevh022@gmail.com>
2025-11-23 16:39:15 +0200

    slots are now Option type insted of SpatialCell, insert/remove/get now return entire slot(position, value), added optional remove_exact / get_exact to get exact positional matches from the map, and a tiny pop speed test case

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,5 @@
-#[derive(Debug, Copy, Clone)]
-enum SpatialCell<T: Copy + Clone> {
-    Occupied([i32; 3], T),
-    Empty,
-}
-
 pub struct SpatialMap<T: Copy + Clone> {
-    data: Box<[SpatialCell<T>]>,
+    data: Box<[Option<([i32; 3], T)>]>,
     dim: [i32; 3],
 }
 
@@ -14,30 +8,44 @@ impl<T: Copy + Clone> SpatialMap<T> {
         assert!(dim.iter().all(|&d| d.is_power_of_two()));
         let len = dim.iter().product::<u32>() as usize;
         Self {
-            data: vec![SpatialCell::Empty; len].into_boxed_slice(),
+            data: vec![None; len].into_boxed_slice(),
             dim: dim.map(|d| d as i32),
         }
     }
 
-    pub fn insert(&mut self, position: [i32; 3], value: T) {
+    pub fn insert(&mut self, position: [i32; 3], value: T) -> Option<([i32; 3], T)> {
         let index = self.index(position);
-        let i3 = unsafe { std::mem::transmute(position) };
-        self.data[index] = SpatialCell::Occupied(i3, value);
+        self.data[index].replace((position, value))
     }
 
-    pub fn remove(&mut self, position: [i32; 3]) {
+    pub fn remove(&mut self, position: [i32; 3]) -> Option<([i32; 3], T)> {
         let index = self.index(position);
-        self.data[index] = SpatialCell::Empty;
+        self.data[index].take()
     }
 
-    pub fn get(&self, position: [i32; 3]) -> Option<&T> {
+    pub fn remove_exact(&mut self, position: [i32; 3]) -> Option<T> {
         let index = self.index(position);
-        match self.data[index] {
-            SpatialCell::Occupied(cell_pos, ref value) if Self::pos_equal(cell_pos, position) => {
-                Some(value)
+        if let Some((cell_pos, _)) = self.data[index] {
+            if Self::pos_equal(cell_pos, position) {
+                return self.data[index].take().map(|(_, v)| v);
             }
-            _ => None,
         }
+        None
+    }
+
+    pub fn get(&self, position: [i32; 3]) -> Option<([i32; 3], T)> {
+        let index = self.index(position);
+        self.data[index]
+    }
+
+    pub fn get_exact(&self, position: [i32; 3]) -> Option<&T> {
+        let index = self.index(position);
+        if let Some((cell_pos, ref value)) = self.data[index] {
+            if Self::pos_equal(cell_pos, position) {
+                return Some(value);
+            }
+        }
+        None
     }
 
     #[inline(always)]
@@ -56,5 +64,33 @@ impl<T: Copy + Clone> SpatialMap<T> {
     #[inline(always)]
     fn pos_equal(a: [i32; 3], b: [i32; 3]) -> bool {
         a[0] == b[0] && a[1] == b[1] && a[2] == b[2]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_map_population() {
+        let cap = 64;
+        let mut map = SpatialMap::<u64>::with_capacity([cap, cap, cap]);
+
+        let time = std::time::Instant::now();
+        let mut i = 0;
+        for _ in 0..1000 {
+            for x in 0..cap {
+                for y in 0..cap {
+                    for z in 0..cap {
+                        let pos = [x as i32, y as i32, z as i32];
+                        map.insert(pos, i);
+                        i += 1;
+                    }
+                }
+            }
+        }
+        let elapsed = time.elapsed();
+
+        println!("{:?}", elapsed);
     }
 }
